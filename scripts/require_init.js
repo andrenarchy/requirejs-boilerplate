0df6c3c2b05b3133e3f8def5d9c7928b8bf0c209
André Gaul <gaul@web-yard.de>
2013-07-23 20:22:15 +0200

    first commit

---
+++ scripts/require_init.js
@@ -0,0 +1,28 @@
+requirejs.config({
+  baseUrl: "scripts",
+  paths: {
+    app: "app/app",
+    backbone: "libs/backbone-min",
+    jquery: "libs/jquery-1.10.2.min",
+    jquerymobile: "libs/jquery.mobile-1.3.2.min",
+    jquerymobileconfig: "app/jquerymobileconfig",
+    pouchdb: "libs/pouchdb-nightly.min",
+    underscore: "libs/underscore-min",
+  },
+  shim: {
+    underscore: {
+      exports: '_'
+    },
+    backbone: {
+      deps: ["underscore", "jquery"],
+      exports: "Backbone"
+    },
+    jquerymobileconfig: ["jquery"],
+    jquerymobile: ["jquery", "jquerymobileconfig"],
+    pouchdb: {
+      exports: "PouchDB"
+    }
+  }
+});
+
+require(["app"]);
